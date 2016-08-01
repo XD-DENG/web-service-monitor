ddaefd59de43df2481e32295758f9d2c82f5a080
XD-DENG <xd_deng@hotmail.com>
2016-08-01 23:47:08 +0800

    remove animation
    This is available as I upgraded ECharts2Shiny package

--- main.R
+++ main.R
@@ -9,10 +9,10 @@ check_interval <- 5
 
 # Server function -------------------------------------------
 server <- function(input, output, sessin) {
-  
+
   dat <- data.frame(0)
   names(dat) <- c("Response.Time")
-  
+
   monitor_log_writer <- observe({
 
     temp <- data.frame(c(runif(1)))
@@ -20,21 +20,24 @@ server <- function(input, output, sessin) {
     row.names(temp) <- c(Sys.time())
 
     dat <<- rbind(dat, temp)
+    if(dim(dat)[1]>20){
+      dat<<-tail(dat, 20)
+    }
     invalidateLater(1000*check_interval, session=getDefaultReactiveDomain())
-    renderLineChart(div_id = "test",
+    renderLineChart(div_id = "test", animation = FALSE,
                     data = dat, line.width = 1, font.size.axis.x = 8, rotate.axis.x = 45)
   })
-  
 
 
-  
+
+
 }
 
 # UI layout -------------------------------------------------
 ui <- fluidPage(
   # We MUST load the ECharts javascript library in advance
   loadEChartsLibrary(),
-  
+
   tags$div(id="test", style="width:100%;height:400px;"),
   deliverChart(div_id = "test")
 )
