f557165759a4fc810289a023c6b6e061c7f8d5f3
XD-DENG <xd_deng@hotmail.com>
2016-08-01 23:13:39 +0800

    initial try

---
+++ main.R
@@ -0,0 +1,43 @@
+library(shiny)
+library(httr)
+library(ECharts2Shiny)
+
+# Prepare sample data for plotting --------------------------
+
+
+check_interval <- 5
+
+# Server function -------------------------------------------
+server <- function(input, output, sessin) {
+  
+  dat <- data.frame(0)
+  names(dat) <- c("Response.Time")
+  
+  monitor_log_writer <- observe({
+
+    temp <- data.frame(c(runif(1)))
+    names(temp) <- c("Response.Time")
+    row.names(temp) <- c(Sys.time())
+
+    dat <<- rbind(dat, temp)
+    invalidateLater(1000*check_interval, session=getDefaultReactiveDomain())
+    renderLineChart(div_id = "test",
+                    data = dat, line.width = 1, font.size.axis.x = 8, rotate.axis.x = 45)
+  })
+  
+
+
+  
+}
+
+# UI layout -------------------------------------------------
+ui <- fluidPage(
+  # We MUST load the ECharts javascript library in advance
+  loadEChartsLibrary(),
+  
+  tags$div(id="test", style="width:100%;height:400px;"),
+  deliverChart(div_id = "test")
+)
+
+# Run the application --------------------------------------
+shinyApp(ui = ui, server = server)
