55dc8e686d45b4571c4bbe86726a046c16642dec
Xiaodong <XD-DENG@users.noreply.github.com>
2016-08-02 09:30:43 +0800

    plug real http request

--- main.R
+++ main.R
@@ -6,26 +6,38 @@ library(ECharts2Shiny)
 
 
 check_interval <- 5
+url_to_monitor <- "http://119.81.97.218/"
 
 # Server function -------------------------------------------
 server <- function(input, output, sessin) {
 
   dat <- data.frame(0)
+  envir_of_dat <- environment()
   names(dat) <- c("Response.Time")
 
   monitor_log_writer <- observe({
 
-    temp <- data.frame(c(runif(1)))
-    names(temp) <- c("Response.Time")
-    row.names(temp) <- c(Sys.time())
-
-    dat <<- rbind(dat, temp)
-    if(dim(dat)[1]>20){
-      dat<<-tail(dat, 20)
+    check_time <- Sys.time()
+    check_result <- try(GET(url_to_monitor))
+    
+    if(class(check_result) != "try-error"){
+      temp_respond_time <- check_result$times['total']
+      print(temp_respond_time)
+    } else {
+      temp_respond_time <- 30
     }
+    
+    temp <- data.frame(c(runif(temp_respond_time)))
+    names(temp) <- c("Response.Time")
+    row.names(temp) <- check_time
+
+    assign('dat',  rbind(dat, temp), envir = envir_of_dat)
+
     invalidateLater(1000*check_interval, session=getDefaultReactiveDomain())
+    
+    # re-plot
     renderLineChart(div_id = "test", animation = FALSE,
-                    data = dat, line.width = 1, font.size.axis.x = 8, rotate.axis.x = 45)
+                    data = dat, line.width = 1, font.size.axis.x = 10, rotate.axis.x = 45)
   })
 
 
